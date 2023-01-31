7653ad2e4adf09df641887f23bc4abc9c0226629
semyonnaumov <semyon.naumov@gmail.com>
2023-01-31 19:10:29 +0400

    Bootstrap project

---
+++ src/main/java/com/naumov/dotnetscriptsscheduler/DotnetScriptsSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.naumov.dotnetscriptsscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotnetScriptsSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotnetScriptsSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naumov/dotnetscriptsscheduler/DotnetScriptsSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naumov.dotnetscriptsscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotnetScriptsSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
