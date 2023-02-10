f8a140fd592cc5a839c8903d15a5d128d32f1a97
semyonnaumov <semyon.naumov@gmail.com>
2023-02-11 00:37:11 +0400

    Fix Kafka deserialization

--- src/main/java/com/naumov/dotnetscriptsscheduler/config/KafkaConsumersConfiguration.java
+++ src/main/java/com/naumov/dotnetscriptsscheduler/config/KafkaConsumersConfiguration.java
@@ -45,7 +45,8 @@ public class KafkaConsumersConfiguration implements KafkaListenerConfigurer {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.naumov.dotnetscriptsscheduler.dto.kafka.cons");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.naumov.*");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
 
         return props;
     }
