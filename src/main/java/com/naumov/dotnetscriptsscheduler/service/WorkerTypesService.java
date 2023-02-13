fc6a6f32ee1df932d51d99d4ecee18631aaf722e
semyonnaumov <semyon.naumov@gmail.com>
2023-02-13 18:32:25 +0400

    Add javadocs to WorkerTypesService

--- src/main/java/com/naumov/dotnetscriptsscheduler/service/WorkerTypesService.java
+++ src/main/java/com/naumov/dotnetscriptsscheduler/service/WorkerTypesService.java
@@ -2,9 +2,23 @@ package com.naumov.dotnetscriptsscheduler.service;
 
 import java.util.Set;
 
+/**
+ * Simple service, used to provide available worker types.
+ */
 public interface WorkerTypesService {
 
+    /**
+     * Returns the set of all worker type names.
+     *
+     * @return set with types
+     */
     Set<String> getAllWorkerTypes();
 
-    boolean workerExists(String agentName);
+    /**
+     * Checks whether the {@code workerType} exists among all available worker types.
+     *
+     * @param workerType worker type to check
+     * @return check result
+     */
+    boolean workerExists(String workerType);
 }
