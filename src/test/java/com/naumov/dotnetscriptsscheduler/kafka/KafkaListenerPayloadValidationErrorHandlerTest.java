7d907105c33128beb061c709d6b40ab4007a9529
semyonnaumov <semyon.naumov@gmail.com>
2023-02-14 20:15:23 +0400

    Add KafkaListenerPayloadValidationErrorHandlerTest

---
+++ src/test/java/com/naumov/dotnetscriptsscheduler/kafka/KafkaListenerPayloadValidationErrorHandlerTest.java
@@ -0,0 +1,26 @@
+package com.naumov.dotnetscriptsscheduler.kafka;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.junit.jupiter.api.Test;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+class KafkaListenerPayloadValidationErrorHandlerTest {
+
+    @Test
+    void handleError() {
+        KafkaListenerPayloadValidationErrorHandler handler = new KafkaListenerPayloadValidationErrorHandler();
+        ListenerExecutionFailedException exceptionMock = mock(ListenerExecutionFailedException.class);
+        when(exceptionMock.getCause()).thenReturn(mock(MethodArgumentNotValidException.class));
+        Acknowledgment ackMock = mock(Acknowledgment.class);
+
+        handler.handleError(mock(Message.class), exceptionMock, mock(Consumer.class), ackMock);
+
+        verify(ackMock, times(1)).acknowledge();
+    }
+}
