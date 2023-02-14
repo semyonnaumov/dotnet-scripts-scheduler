e9e847d177be28e677eab0cca565f84469b839cf
semyonnaumov <semyon.naumov@gmail.com>
2023-02-14 13:32:03 +0400

    Finish DotnetScriptsSchedulerApplicationTest

--- src/test/java/com/naumov/dotnetscriptsscheduler/DotnetScriptsSchedulerApplicationTest.java
+++ src/test/java/com/naumov/dotnetscriptsscheduler/DotnetScriptsSchedulerApplicationTest.java
@@ -1,21 +1,82 @@
 package com.naumov.dotnetscriptsscheduler;
 
 import com.naumov.dotnetscriptsscheduler.repository.JobsRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@DirtiesContext
+@AutoConfigureMockMvc
 class DotnetScriptsSchedulerApplicationTest extends AbstractIntegrationTest {
     @Autowired
     private JobsRepository jobsRepository;
+    @Autowired
+    private MockMvc mvc;
 
     @Transactional
     @Test
     public void contextLoads() {
         assertTrue(jobsRepository.findJobRequestByJobId(UUID.randomUUID()).isEmpty());
+    }
+
+    @Test
+    public void actuatorHealthy() throws Exception {
+        mvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", Matchers.is("UP")));
+    }
+
+    @Test
+    public void swaggerUiUp() throws Exception {
+        mvc.perform(get("/swagger-ui/index.html"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string(containsString("Swagger UI")));
+    }
+
+
+    @Test
+    public void jobCreates() throws Exception {
+        String jobCreateRequest = """
+                {
+                    "requestId": "request-0",
+                    "senderId": "sender-0",
+                    "payload": {
+                        "script": "Console.WriteLine(\\"Hello from from job from sender-0\\");",
+                        "agentType": "linux-amd64-dotnet-7"
+                    }
+                }
+                """;
+
+        MockHttpServletRequestBuilder jobCreationRequest = post("/jobs")
+                .content(jobCreateRequest.getBytes(StandardCharsets.UTF_8))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mvc.perform(jobCreationRequest)
+                .andExpect(status().isCreated())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.jobId", notNullValue()));
+
+        // second request with similar requestId must have response status code 200
+        mvc.perform(jobCreationRequest)
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.jobId", notNullValue()));
     }
 }
